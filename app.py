7694bb7e69f63f197dbe6a9969e5223021d97f2f
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-28 16:03:18 +0330

    Add Config and Datebase

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Configs
 
 app = Flask(__name__)
+app.config.from_object(Configs)
+
+db = SQLAlchemy()
+db.init_app(app=app)
+
+migrate = Migrate(app, db)
+
 
 from mod_admin import admin
 from mod_app import todo

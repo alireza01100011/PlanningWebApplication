c32a54883bd22cfbd2d0631060834d553e422907
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-30 22:50:28 +0330

    update base login and register [route]

--- mod_user/routes.py
+++ mod_user/routes.py
@@ -2,21 +2,27 @@ from flask import (
                     render_template, abort, request)
 
 
-from . import user, User
+from . import user
+from .models import User
+from .forms import LoginForm, RegisterForm
 
 from app import db, bcrypt
 
 @user.route('login/', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     if request.method == 'POST':
         pass
 
-    return render_template('user/login.html', title='Login')
+    return render_template('user/login.html', title='Login',
+                           form=form)
 
 
 @user.route('register/', methods=['GET', 'POST'])
 def register():
+    form = RegisterForm()
     if request.method == 'POST':
         pass
 
-    return render_template('user/register.html', title='Register')
+    return render_template('user/register.html', title='Register',
+                           form=form)

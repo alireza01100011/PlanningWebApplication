ccfc34c4f89679a186de20008bdd116dcb88cdad
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-31 17:48:27 +0330

    fix Bug

--- dateabase_models/_models.py
+++ dateabase_models/_models.py
@@ -6,7 +6,7 @@ from app import db, login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'

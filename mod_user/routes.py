3d78e9d12123b562ef9c8dc6368f4a2f25430d61
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-31 17:49:39 +0330

    route - register user

--- mod_user/routes.py
+++ mod_user/routes.py
@@ -1,6 +1,8 @@
-from flask import (
+from flask import ( flash, url_for, redirect,
                     render_template, abort, request)
+from flask_login import login_user, logout_user, login_required, current_user
 
+from sqlalchemy.exc import IntegrityError
 
 from . import user
 from .models import User
@@ -8,11 +10,18 @@ from .forms import LoginForm, RegisterForm
 
 from app import db, bcrypt
 
+@user.route('/', methods=['GET'])
+def profile():
+    if not current_user.is_authenticated:
+        return redirect(url_for('user.login', next=url_for('user.profile')))
+    return f'profile <br> Hi,{current_user.full_name}'
+
 @user.route('login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if request.method == 'POST':
-        pass
+        if not form.validate_on_submit():
+            return render_template('user/login.html', title='Login', form=form)
 
     return render_template('user/login.html', title='Login',
                            form=form)
@@ -22,7 +31,26 @@ def login():
 def register():
     form = RegisterForm()
     if request.method == 'POST':
-        pass
+        if not form.validate_on_submit():
+            return render_template('user/login.html', title='Login', form=form)
+
+        NewUser = User(
+            full_name=form.full_name.data,
+            email=form.email.data,
+            password=bcrypt.generate_password_hash(
+                password=form.password.data)
+            )
+        
+        try:
+            db.session.add(NewUser)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash('Error! Try again (probably because the email you entered already exists)')
+            return render_template('user/register.html', title='Register', form=form)
+        else:
+            flash('Your account has been created successfully')
+            return redirect(url_for('user.login'))
 
     return render_template('user/register.html', title='Register',
                            form=form)

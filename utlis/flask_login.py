22c1bc33343591e5ba1badfc57b8b16cb2ca6ba9
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-31 19:12:06 +0330

    Add message flash when the user is logged in

--- utlis/flask_login.py
+++ utlis/flask_login.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import redirect, url_for
+from flask import redirect, url_for, flash
 from flask_login import current_user
 
 
@@ -11,7 +11,7 @@ def not_logged_in(_redirect:str):
         def wrapper(*args, **kwargs):
             if not current_user.is_authenticated:
                 return func(*args, **kwargs)
-            
+            flash('You are already logged in .')
             return redirect(url_for(_redirect))
         
         return wrapper

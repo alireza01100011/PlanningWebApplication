a48aa7386bdf77e3238a3077e2be2f6a0806e6bf
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-08 21:21:26 +0330

    Remove additional fields and rename

--- mod_application/task/forms.py
+++ mod_application/task/forms.py
@@ -4,8 +4,6 @@ from wtforms.fields import StringField, TextAreaField, SelectField
 
 from utlis.forms import _get_fields, MultiCheckboxField
 
-from mod_user.user.forms import User
-
 
 class TaskForm(FlaskForm):
     title = StringField(description='Add Title :',
@@ -14,26 +12,19 @@ class TaskForm(FlaskForm):
     description = TextAreaField(description='Add Description',
                                 validators=(Length(1, 2048),))
     
-    calendar = SelectField(label='Select The Calendar', choices=['Tasks'])
+    group = SelectField(label='Select The Task Group')
 
-    _list_times = [
-        f'{h}:{m}' for h in range(0,24)
-          for m in range(0, 46, 15) ]
-
-    start = SelectField(description='Start Time',
-                        validators=(DataRequired(),), choices=_list_times)
+    start = SelectField(description='Start Time', validators=(DataRequired(),))
     
-    end = SelectField(description='End Time', choices=_list_times)
     
     #  [(1, 5)]+[(, reminder_t) for reminder_t in range(15, 61, 15)]
-    def _list_choices()->list[tuple[int, int]]:
-        count, choices = 2, [2, '5']
-        for reminder_t in range(15, 61, 15):
-            count += 1
-            choices.append((count, f'{reminder_t}'))
-        return choices
-    reminder = MultiCheckboxField(description='Reminder Before The Event',
-                                  coerce=int, choices=_list_choices()) 
+    # def _list_choices()->list[tuple[int, int]]:
+    #     count, choices = 2, [2, '5']
+    #     for reminder_t in range(15, 61, 15):
+    #         count += 1
+    #         choices.append((count, f'{reminder_t}'))
+    #     return choices
+    reminder = MultiCheckboxField(label='Reminder', description='Reminder Before The Event') 
 
     def get_fields(self):
         return _get_fields(self)

8e44a0eb043b6d88a36f32c0571be493f0b879fd
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-08 23:06:50 +0330

    Added Route "Edit Group"

--- mod_application/group/routes.py
+++ mod_application/group/routes.py
@@ -51,6 +51,47 @@ def add():
 
 @groups.route('/edit/<int:id>')
 def edit(id:int):
+    form = GroupForm()
+    group_manager = GroupManager()
+    group_manager.set_groups(loads(current_user.groups))
+    
+    _group = group_manager.groups.get(int(id))
+    if not  _group :
+        del group_manager, _group # Free up the RAM
+        return abort(404)
+
+    if request.method == 'GET':
+        form.title.data = _group.title
+        form.description.data = _group.description
+        form.color.data = _group.color
+
+        del group_manager # Free up the RAM
+    del _group # Free up the RAM
+
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            flash('Invalid forum', category='error')
+            return render_template('', title='Edit Group', form=form)
+        
+        group_manager.update_group(
+            id=int(id),
+            title=form.title.data,
+            color=form.color.data,
+            description=form.description.data)
+
+        current_user.group = group_manager.return_group_in_pickle
+        del group_manager # Free up the RAM
+        
+        try:
+            db.session.commit()
+            
+        except IndentationError:
+            db.session.rollback()
+            flash('Something went wrong, please try again', category='error')
+        
+        else:
+            flash('Group edited successfully', category='success')
+            redirect(url_for('group.manage'))
 
     return render_template('', title='Edit Group', form=form)
 

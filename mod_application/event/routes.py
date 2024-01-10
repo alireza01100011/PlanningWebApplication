5bd33403a3329de1586b3adb50e786d6c217f9f2
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-10 20:42:27 +0330

    Add Route "Edit Event"

--- mod_application/event/routes.py
+++ mod_application/event/routes.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 from . import event as blueprint_event
 from .forms import EventForm
 from .models import Event as db_event
-from ..memory_management.event import EventManager
+from ..memory_management.event import EventManager, _Event
 from ..memory_management.group import GroupManager
 from ...mod_user.user.models import User as db_user
 
@@ -27,7 +27,7 @@ def add():
     form = EventForm()
     
     group_manager = GroupManager(
-        pickle_data=current_user.events.events)
+        pickle_data=current_user.groups)
 
     if request.method == 'GET':
         # Fill in the group field
@@ -81,7 +81,78 @@ def add():
 
 @blueprint_event.route('/edit', methods=['GET', 'POST'])
 def edit():
-    return 'edit'
+    form = EventForm()
+    current_user:db_user = current_user
+    
+    event_manager = EventManager(
+        pickle_data=current_user.event.events)
+    
+    _selected_event:_Event|None = \
+        event_manager.events.get(int(id))
+    
+    # EventNot Found 
+    if not _selected_event:
+        return abort(404)
+    # ----
+
+    group_manager = GroupManager(
+        pickle_data=current_user.groups)
+
+
+    if request.method == 'GET':
+        # Fill in the group field
+        form.group.choices = [
+            (group.id, group.title)
+            for group in group_manager.list_groups]
+        
+        form.title.data = _selected_event.title
+        form.end.data = _selected_event.end_time
+        form.group.data = _selected_event.group_id
+        form.start.data = _selected_event.start_time
+        form.reminder.data = _selected_event.reminders
+        form.description.data = _selected_event.description
+
+    # ----
+
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            flash('Invalid forum', category='error')
+            return render_template('',
+                                title=' Event ', form=form)
+        # ----
+        
+        _group_id = 0 # Default Group ID
+        if group_manager.groups.get(int(id)):
+            _group_id = form.group.data
+        # ----
+            
+        # Update Event
+        event_manager.update_event(
+            id=int(id),
+            title=form.title.data,
+            description=form.description.data,
+            start_time=form.start.data,
+            reminders=form.reminder.data,
+            group_id=_group_id)
+        
+        # Seve Data In Pickle
+        current_user.events.events = event_manager.return_events_in_pickle
+
+        try:
+            db.session.commit()
+            
+        except IndentationError:
+            db.session.rollback()
+            flash('Something went wrong, please try again', category='error')
+        
+        else:
+            flash('Event edited successfully', category='success')
+            redirect(url_for('event.manage'))
+    # ----
+    
+    return render_template('', 
+                    title=' Event ', form=form)
+
 
 @blueprint_event.route('/delete', methods=['GET'])
 def remove():

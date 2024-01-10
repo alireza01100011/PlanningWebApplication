2b6452ffa9acd527d55f26a16bac5d089f68ef40
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-10 19:53:02 +0330

    add route "Add Event"

--- mod_application/event/routes.py
+++ mod_application/event/routes.py
@@ -3,6 +3,7 @@ from flask_login import current_user
 from sqlalchemy.exc import IntegrityError
 
 from . import event as blueprint_event
+from .forms import EventForm
 from .models import Event as db_event
 from ..memory_management.event import EventManager
 from ...mod_user.user.models import User as db_user
@@ -21,7 +22,45 @@ def manage():
 
 @blueprint_event.route('/add', methods=['GET', 'POST'])
 def add():
-    return 'add'
+    current_user:db_user = current_user
+    form = EventForm()
+    
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            flash('Invalid forum', category='error')
+            return render_template('',
+                                title=' Event ', form=form)
+        # --- End IF ----
+
+        event_manager = EventManager(
+                pickle_data=current_user.event.events)
+        
+        # Add New Event
+        event_manager.add_event(
+            title=form.title.data,
+            description=form.description.data,
+            start_time=form.start.data,
+            end_time=form.end.data,
+            reminders=form.reminder.data)
+        
+        # Seve Data In Pickle
+        current_user.events.events = event_manager.return_events_in_pickle
+
+        try:
+            db.session.commit()
+            
+        except IndentationError:
+            db.session.rollback()
+            flash('Something went wrong, please try again', category='error')
+        
+        else:
+            flash('Event add successfully', category='success')
+            redirect(url_for('event.manage'))
+
+    return render_template('', 
+                    title=' Event ', form=form)
+
+
 
 @blueprint_event.route('/edit', methods=['GET', 'POST'])
 def edit():

8255ed50616a716c059b595b359f8fc40b1483c6
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-10 14:47:52 +0330

    Added Route "Edit Task"

--- mod_application/task/routes.py
+++ mod_application/task/routes.py
@@ -71,3 +71,64 @@ def add():
     return render_template('', 
                         title='Task', form=form)
 
+
+@task.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id:int):
+    form = TaskForm()
+    
+    user:User = current_user
+    user_tasks:Task = user.tasks
+
+    task_manager = TasksManager()
+    task_manager.set_tasks(pickle.loads(user_tasks.tasks))
+    _task_selected = task_manager.tasks.get(int(id))
+
+    group_manager = GroupManager()
+    group_manager.set_groups(pickle.loads(user.groups))
+
+    if not _task_selected:
+        return abort(404)
+
+    if request.method == 'GET':
+        form.group.choices = [
+            (group.id, group.title)
+            for group in group_manager.list_groups]
+        
+        form.title.data = _task_selected.name
+        form.start.data = _task_selected.time_start
+        form.group.data = _task_selected.group_id
+
+
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            flash('The form is invalid', category='error')
+            return render_template('', 
+                                    title='Edit Task', form=form)
+        
+        # Does this group exist? If not, select the default group
+        _group = 0 # Defualt ID
+        if group_manager.groups.get(form.group.data):
+            _group = form.group.data
+
+
+        task_manager.update_task(
+            id = int(id),
+            name = form.title.data,
+            time_start= form.start.data,
+            group_id = _group)
+        
+        user_tasks.tasks = task_manager.return_tasks_in_pickle
+
+        try:
+            db.session.commit()
+
+        except IndentationError:
+            db.session.rollback()
+            flash('Task editing failed! Please try again', category='error')
+        
+        else:
+            flash('The task was edited successfully', category='success')
+            return redirect(url_for('task.manage'))
+            
+    return render_template('', 
+                        title='Edit Task', form=form)

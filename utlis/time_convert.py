8fc1840266b9ba97486024705795e9ae1c7b0cab
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-31 18:54:23 +0330

    update

--- utlis/time_convert.py
+++ utlis/time_convert.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
-def string_to_int(_time:str, fromat:str='%Y-%m-%d %H:%M')->str :
-    res = datetime.strptime(_time, "%Y-%m-%d %H:%M").timestamp()
-    return f'{int(res)}000'
+def string_to_int(_time:str, format:str='%Y-%m-%d %H:%M', _add:str='000')->str :
+    res = datetime.strptime(_time, f'{format}').timestamp()
+    return f'{int(res)}{_add}'

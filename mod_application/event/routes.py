068275bac5fbd231c7d87bcaa4b8cdedb3ad484f
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-31 16:35:44 +0330

    Sending events as JSON and adding new events

--- mod_application/event/routes.py
+++ mod_application/event/routes.py
@@ -1,5 +1,5 @@
 import sys
-from flask import render_template, request, abort, flash, redirect, url_for
+from flask import render_template, request, abort, flash, redirect, url_for, jsonify
 from flask_login import current_user
 from sqlalchemy.exc import IntegrityError
 
@@ -10,6 +10,7 @@ from ..memory_management.event import EventManager, _Event
 from ..memory_management.group import GroupManager
 
 from utlis.flask_login import login_required
+from utlis.time_convert import string_to_int as time_s_i
 
 sys.path.append("../..")
 from mod_user.user.models import User as db_user
@@ -28,7 +29,7 @@ def manage():
     
     # Fill in the group field
     groups = [
-        (group.title, group.color, group.id)
+        (group.title, group.color)
         for group in group_manager.list_groups]
 
     # manager_event = EventManager(
@@ -42,7 +43,7 @@ def manage():
 
 
 @blueprint_event.route('/add', methods=['GET', 'POST'])
-
+@login_required()
 def add():
 
     if request.method == 'GET':
@@ -54,32 +55,33 @@ def add():
 
     if request.method == 'POST':
         form_data = request.get_json()
-
+        
         if not validate_event_form(form_data):
             return abort(400)
         # --- End IF ----
+
+        extendedProps:dict = form_data.get('extendedProps')
         
         event_manager = EventManager(
                 pickle_data=user.events[0].events)
+
         
-        _group_id = 0 # Default Group ID
-        
-        # try: int(form_data['lable'])
-        # except KeyError or ValueError: pass
-        # else: _id = int(form_data['lable'])
-        
-        # if group_manager.groups.get(_id):
-        #     _group_id = _id
+        group_title = 0 # Default Group ID
+        _calendar = extendedProps.get('calendar')
+        if group_manager.groups.get(_calendar):
+            group_title = _calendar
         
         # Add New Event
         event_manager.add_event(
-            color='000',
             title=form_data.get('title'),
+            url=form_data.get('url'),
             description=form_data.get('description'),
-            start_time=form_data.get('start'),
-            end_time=form_data.get('end'),
+            start_time= \
+                time_s_i(form_data.get('start')),
+            end_time= \
+                time_s_i(form_data.get('end')),
             reminders=form_data.get('reminders') or [],
-            group=_group_id)
+            group=group_title)
         
         # Seve Data In Pickle
         user.events[0].events = event_manager.return_events_in_pickle
@@ -95,7 +97,7 @@ def add():
             flash('Event add successfully', category='success')
             redirect(url_for('event.manage'))
         
-        return 'OK'
+        return f'{event_manager.events}'
 
 @blueprint_event.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id:int):
@@ -206,5 +208,39 @@ def remove(id:int):
     return redirect(url_for('event.manage'))
     
 
+@blueprint_event.route('/json/get', methods=['GET'])
+@login_required()
+def send_json():
+    from time import time
+    user:db_user = db_user.query.get(current_user.id)
+    
+    group_manager = GroupManager(
+        pickle_data=user.groups)
 
-
+    manager_event = EventManager(
+        pickle_data=user.events[0].events)
+    manager_event.add_event(
+        'Done Project',
+        'des',
+        time() - 3600,
+        time(),
+        None,
+        reminders=[],
+        url='https://google.com',
+        all_day=True
+    )
+    response = [
+        {
+            "id": event.id, 
+            "url":  event.url, 
+            "title": event.title, 
+            "start": int(event.start_time), 
+            "end": int(event.end_time), 
+            "allDay": event.all_day, 
+            "extendedProps": { 
+                "calendar": group_manager.groups[event.group_title].title
+            } 
+        }
+        for event in manager_event.events.values()
+    ]
+    return jsonify(response)

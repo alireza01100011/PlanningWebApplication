c91906c9142bb0b1b44bacd024ed0bdb51dd7a8f
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-31 16:40:44 +0330

    for DEV

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from os import getenv, urandom
 
 class Configs():
     SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
-    SECRET_KEY = urandom(64)
+    SECRET_KEY = 'klajsdfjasdl;fkjas;dlfkjas;ldfjas;lkdj'

6942292828f3d266210bea84a40ce703c0d67ed3
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-01-31 16:36:47 +0330

    Receive events

--- static/assets/js/app-calendar-events.js
+++ static/assets/js/app-calendar-events.js
@@ -1,122 +1,35 @@
-"use strict";
-
+"use strict"; 
 let date = new Date,
-    nextDay = new Date((new Date).getTime() + 864e5),
-    nextMonth = 11 === date.getMonth() ? new Date(date.getFullYear() + 1, 0, 1) : new Date(date.getFullYear(), date.getMonth() + 1, 1),
-    prevMonth = 11 === date.getMonth() ? new Date(date.getFullYear() - 1, 0, 1) : new Date(date.getFullYear(), date.getMonth() - 1, 1),
-    events = [
-    {
-        id: 1,
-        url: "",
-        title: "Design Review",
-        start: date,
-        end: nextDay,
-        allDay: !1,
-        extendedProps: {
-            calendar: "Business"
-        }
-    },
-    
-    {
-        id: 2,
-        url: "",
-        title: "Meeting With Client",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -11),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -10),
-        allDay: !0,
-        extendedProps: {
-            calendar: "Business"
-        }
-    }, {
-        id: 3,
-        url: "",
-        title: "Family Trip",
-        allDay: !0,
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -9),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -7),
-        extendedProps: {
-            calendar: "Holiday"
-        }
-    }, {
-        id: 4,
-        url: "",
-        title: "Docsdfsdf tor's Appointment",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -11),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -10),
-        extendedProps: {
-            calendar: "Personal"
-        }
-    }, {
-        id: 5,
-        url: "",
-        title: "Dart Game?",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -13),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -12),
-        allDay: !0,
-        extendedProps: {
-            calendar: "ETC"
-        }
-    }, {
-        id: 6,
-        url: "",
-        title: "Meditation",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -13),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -12),
-        allDay: !0,
-        extendedProps: {
-            calendar: "Personal"
-        }
-    }, {
-        id: 7,
-        url: "",
-        title: "Dinner",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -13),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -12),
-        extendedProps: {
-            calendar: "Family"
-        }
-    }, {
-        id: 8,
-        url: "",
-        title: "Product Review",
-        start: new Date(date.getFullYear(), date.getMonth() + 1, -13),
-        end: new Date(date.getFullYear(), date.getMonth() + 1, -12),
-        allDay: !0,
-        extendedProps: {
-            calendar: "Business"
-        }
-    }, {
-        id: 9,
-        url: "",
-        title: "Monthly Meeting",
-        start: nextMonth,
-        end: nextMonth,
-        allDay: !0,
-        extendedProps: {
-            calendar: "Business"
-        }
-    }, {
-        id: 10,
-        url: "",
-        title: "Monthly Checkup",
-        start: prevMonth,
-        end: prevMonth,
-        allDay: !0,
-        extendedProps: {
-            calendar: "Personal"
-        }
-    },
+nextDay = new Date((new Date).getTime() + 864e5),
+nextMonth = 11 === date.getMonth() ? new Date(date.getFullYear() + 1, 0, 1) : new Date(date.getFullYear(),
+date.getMonth() + 1, 1), prevMonth = 11 === date.getMonth() ? new Date(date.getFullYear() - 1, 0, 1) : new Date(date.getFullYear(), date.getMonth() - 1, 1);
 
-    {
-        id: 11,
-        url: "",
-        title: "asdfsdfsdfasdfsdfw",
-        start:  new Date(2024, 1 + 26),
-        end: new Date(2024, 1 + 26),
-        allDay: !1,
-        extendedProps: {
-            calendar: "Business"
-        }
-    },
+var events = [];
 
-];
+xhr = new XMLHttpRequest();
+xhr.open("GET", '/event/json/get', true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.onload = function () {
+    let data = JSON.parse(this.responseText);
+
+    for (let key in data){
+        let e = data[key];
+        let NewEvent = {
+            id: e.id,
+            url: "",
+            title: e.title,
+            start:  new Date(e.start),
+            end: new Date(e.end ),
+            allDay: e.allDay,
+            extendedProps: e.extendedProps
+        };
+        
+        events.push(NewEvent);
+    };
+
+    let buttonselectAll = document.getElementById('selectAll');
+    buttonselectAll.click();buttonselectAll.click();
+}
+
+xhr.send();
+console.log(events)

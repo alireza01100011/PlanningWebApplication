943ad83ea76ab38b17fc14fdd95f1794c91fe223
Alireza Mosaveyan <Armanparsi206@outlook.com>
2024-02-05 20:00:03 +0330

    Update Group Form

--- mod_application/group/forms.py
+++ mod_application/group/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length
-from wtforms.fields import StringField, TextAreaField, ColorField
+from wtforms.fields import StringField, TextAreaField, SelectField
 
 from utlis.forms import _get_fields
+from utlis.dictionary import COLORs as COLORS
 
-
+COLORs = COLORS()
 class GroupForm(FlaskForm):
     title = StringField(description='Add Title',
                         validators=(DataRequired(), Length(1, 128)))
@@ -12,7 +13,13 @@ class GroupForm(FlaskForm):
     description = TextAreaField(description='Add Description',
                                 validators=(Length(1, 2048),))
     
-    color = ColorField(label='Select Group Color')
+    color = SelectField(
+        label='Select Group Color',
+        choices=[
+            (key, name)
+            for key, name in COLORS.colors_bootstrap.items()
+        ]
+    )
 
     
     def get_fields(self):
